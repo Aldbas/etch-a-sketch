54306e64966b21483bd99eb525a0332518620e1d
AldBas <bascugaldwin@gmail.com>
2019-09-04 10:37:17 -0700

    hovering on squares changes color

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-window.addEventListener('DOMContentLoaded', gridAdd);
-const gridContainer = document.getElementById('container');
+// window.addEventListener('load', gridAdd);
+const gridContainer = document.getElementById('grid-container');
 
 function gridAdd() {
     for (i = 1; i <= 256; i++) {
         let gridSquare = document.createElement('div');
-        let gridText = document.createTextNode(i);
-        gridSquare.appendChild(gridText);
+        // let gridText = document.createTextNode(i);
+        // gridSquare.appendChild(gridText);
         gridContainer.appendChild(gridSquare);
-        gridSquare.classList.add('penis');
     }
+    // callback();
 }
-let test = document.getElementsByClassName('penis');
-let arr = [...test];
-console.log(arr);
+function addHover() {
+let hover = document.querySelectorAll('#grid-container div');
+hover.forEach(grid => grid.addEventListener('mouseover', function(){
+	console.log('PENIS');
+this.classList.add('penis');
+}));
+console.log(hover);
+}
+gridAdd();
+addHover();
+
+
