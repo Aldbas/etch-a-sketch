14defe7285ddcb10eb07f617e2eaa1401c533218
AldBas <bascugaldwin@gmail.com>
2019-09-04 11:31:38 -0700

    highlight on hover

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const gridContainer = document.getElementById('grid-container');
 function gridAdd() {
     for (i = 1; i <= 256; i++) {
         let gridSquare = document.createElement('div');
-        // let gridText = document.createTextNode(i);
-        // gridSquare.appendChild(gridText);
+        let gridText = document.createTextNode(i);
+        gridSquare.appendChild(gridText);
         gridContainer.appendChild(gridSquare);
     }
     // callback();
 }
 function addHover() {
-let hover = document.querySelectorAll('#grid-container div');
+let hover = document.querySelectorAll('div');
 hover.forEach(grid => grid.addEventListener('mouseover', function(){
 	console.log('PENIS');
 this.classList.add('penis');
