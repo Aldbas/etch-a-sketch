984b1d078f6210df1e9a29253f2742231ab65af2
AldBas <bascugaldwin@gmail.com>
2019-09-06 11:37:01 -0700

    gradually gets darker on hover

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const gridContainer = document.getElementById('grid-container');
 let gridSize = 16;
 
 function gridCreate() {
-    gridContainer.style.gridTemplate = `repeat(${gridSize}, auto) / repeat(${gridSize},auto)`;
+    gridContainer.style.gridTemplate = `repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr)`;
     for (i = 0; i < (gridSize * gridSize); i++) {
         let gridSquare = document.createElement('div');
+        gridSquare.className = 'square';
+        gridSquare.style.backgroundColor = "purple";
+        gridSquare.style.opacity = "0.0";
         gridContainer.appendChild(gridSquare);
     }
     addHover();
@@ -13,29 +16,31 @@ function gridCreate() {
 
 function addHover() {
     let hover = document.querySelectorAll('#grid-container div');
-    hover.forEach(grid => grid.addEventListener('mouseover', function() {
-        this.classList.add('penis');
-    }));
-}
-function clearSquares() {
-	let square = document.getElementById('grid-container');
-	while(square.firstChild) {
-			square.removeChild(square.firstChild);
+    hover.forEach(grid => grid.addEventListener('mouseover', changingGrayScale));
 
-	} 
 }
+
+function changingGrayScale(e) {
+	e.target.style.opacity = parseFloat(e.target.style.opacity) + 0.3;
+}
+
+
 
 let resizeBtn = document.querySelector('button.resize');
 resizeBtn.addEventListener('click', resize);
 
-let resetBtn = document.querySelector('button.clear');
-resetBtn.addEventListener('cick', clearSquares);
+let clearBtn = document.querySelector('button.clear');
+clearBtn.addEventListener('click', clearSquares);
+
+function clearSquares() {
+    let squares = document.getElementById('grid-container').innerHTML = '';
+    gridCreate();
+}
 
 function resize() {
-	console.log(gridSize);
-	gridSize = prompt('Enter a new number to resize grid');
-	clearSquares();
-	gridCreate();
-	addHover();
+    gridSize = prompt('Enter a new number to resize grid');
+    clearSquares();
+    gridCreate();
+    addHover();
 }
 gridCreate();
